70bd5a344d344fc96dbba0fa88cd6783bdcec68b
unknown <a.akshabaev@c441a-10.kbtu.kz>
2017-01-23 10:57:35 +0600

    Week 2 group 1 lecture

---
+++ Example1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    /// <summary>
+    /// Complex class example
+    /// </summary>
+    class Complex
+    {
+        public int x, y;
+        public Complex(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(x + "/" + y);
+        }
+
+        public override string ToString()
+        {
+            return x + "/" + y;
+        }
+
+        // (1, 2), (2,3) => (3, 5)
+        public Complex Add(Complex c) // this -> a, c -> b
+        {
+            Complex d = new Complex(this.x + c.x, this.y + c.y);
+            return d;
+        }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex c = new Complex(c1.x + c2.x, c1.y + c2.y);
+            return c;
+        }
+    }
+    /// <summary>
+    /// Student class example
+    /// </summary>
+    class Student
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public double gpa;
+
+        public Student(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public Student(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+        public override string ToString()
+        {
+            return this.name + ": " + this.surname + ": " + this.age;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex a = new Complex(1, 2);
+            Complex b = new Complex(2, 3);
+
+            Complex c = a + b + a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+            
+            /*
+            Student a = new Student("Marat", "Abuov");
+            Student b = new Student("aaa", "bbb", 20);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadKey();
+             */ 
+        }
+    }
+}
---
+++ Example2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Example2
+{
+    /// <summary>
+    /// DirectoryInfo
+    /// FileInfo
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Ex1()
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"c:\Windows");
+            Console.WriteLine(directory.Name);
+            Console.WriteLine(directory.FullName);
+            Console.ReadKey();
+        }
+        static void Ex2()
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"c:\Windows");
+            // Getting files.
+            FileInfo[] files = directory.GetFiles();
+
+            // Getting directories
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            Console.WriteLine(files.Length);
+            Console.WriteLine(directories.Length);
+            Console.ReadKey();
+        }
+        static void Ex3()
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"c:\Windows");
+            // Getting files.
+            FileInfo[] files = directory.GetFiles();
+
+            // Getting directories
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            Console.WriteLine("Files:");
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Directories:");
+
+            foreach (DirectoryInfo dInfo in directories)
+            {
+                Console.WriteLine(dInfo.Name);
+            }
+            Console.ReadKey();
+        }
+
+        static void emptySpace(int level)
+        {
+            for (int i = 0; i < level * 2; i++)
+                Console.Write(" ");
+        }
+
+        static void Ex4(string path, int level)
+        {
+            if (level > 2)
+                return;
+            try
+            {
+                DirectoryInfo directory = new DirectoryInfo(path);
+                FileInfo[] files = directory.GetFiles();
+                DirectoryInfo[] directories = directory.GetDirectories();
+
+                foreach (FileInfo file in files)
+                {
+                    emptySpace(level);
+                    Console.WriteLine(file.Name);
+                }
+                foreach (DirectoryInfo dInfo in directories)
+                {
+                    emptySpace(level);
+                    Console.WriteLine(dInfo.Name);
+                    Ex4(dInfo.FullName, level + 1);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Ex4(@"C:\Windows", 0);
+        }
+    }
+}
